4707457d95285475442e3d86b799dab1f380b654
daniel <danielpchelincev987@mail.ru>
2026-09-02 21:41:17 +0300

    поменял тип на запрос

--- src/features/searchDishes/api/getAllDishes.ts
+++ src/features/searchDishes/api/getAllDishes.ts
@@ -1,5 +1,6 @@
+import { Order } from '@/entities/order/model/types';
 import httpClient from '@/shared/api/httpClient';
 
-export const getAllDishes = async (data: object): Promise<string[]> => {
-    return await httpClient.post<string[]>('Orders/optimize', data);
+export const getAllDishes = async (data: object): Promise<Order[]> => {
+    return await httpClient.post<Order[]>('Orders/optimize', data);
 };
