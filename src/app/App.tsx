76fe2b471b80f7fdbafcba8b496e9c979523edd3
daniel <danielpchelincev987@mail.ru>
2026-09-02 21:38:01 +0300

    добавлен новый роут на страницу варианта заказа

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,7 +4,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import FiltersPage from '@/pages/filtersPage/FiltersPage.js';
 import MainPage from '@/pages/mainPage/MainPage.js';
-import ResultPage from '@/pages/resultPage/ResultPage';
+import OrderItemPage from '@/pages/OrderItemPage/OrderItemPage';
+import OrderPage from '@/pages/orderPage/OrderPage';
 import Header from '@/widgets/Header/ui/Header.js';
 
 const queryClient = new QueryClient();
@@ -24,7 +25,8 @@ function App() {
                     <Routes>
                         <Route path="/" element={<MainPage />} />
                         <Route path="/filters" element={<FiltersPage />} />
-                        <Route path="/dishlist" element={<ResultPage />} />
+                        <Route path="/dishlist" element={<OrderPage />} />
+                        <Route path="/dishlist/:id" element={<OrderItemPage />} />
                     </Routes>
                 </BrowserRouter>
             </QueryClientProvider>
