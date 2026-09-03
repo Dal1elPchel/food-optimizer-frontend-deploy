e7ac965673228292b3953ded45d2f414c27e823b
daniel <danielpchelincev987@mail.ru>
2026-09-03 16:17:30 +0300

    добавил отображение на компонент списка позиций заказа

--- src/entities/orderItem/UI/OrderItemComponent.tsx
+++ src/entities/orderItem/UI/OrderItemComponent.tsx
@@ -1,5 +1,27 @@
+import { Order } from '@/entities/order/model/types';
+
 import styles from './OrderItem.module.scss';
 
-const OrderItemComponent = (props: any) => {};
+interface OrderItemProps {
+    order: Order;
+}
+
+const OrderItemComponent = ({ order }: OrderItemProps) => {
+    return (
+        <article className={styles.OrderItemCard}>
+            <h4 className={styles.title}>Что в заказе</h4>
+
+            <ul className={styles.orderItemList}>
+                {order.OrderItems.map((orderItem, index) => (
+                    <li key={index} className={styles.orderItem}>
+                        <span>{orderItem.itemName}</span>
+                        <span>{orderItem.itemPrice}</span>
+                        <span>{orderItem.calories}</span>
+                    </li>
+                ))}
+            </ul>
+        </article>
+    );
+};
 
 export default OrderItemComponent;
