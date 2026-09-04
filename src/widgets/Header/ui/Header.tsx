751af1ff6867de2cfdc394f9b8d1b2229241b850
daniel <danielpchelincev987@mail.ru>
2026-09-04 18:52:04 +0300

    использовал модалку на header

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import { useThemeStore } from '@/shared/lib/theme/useThemeStore';
+import { AboutModal } from '@/widgets/AboutModal/ui/aboutModal';
 
 import Logo from '../../../shared/assets/icons/Logo.png';
 import styles from './Header.module.scss';
@@ -11,58 +12,66 @@ const Header = () => {
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
     const theme = useThemeStore((s) => s.currentTheme);
     const onThemeToggle = useThemeStore((s) => s.toggleTheme);
+    const [aboutModalOpen, setAboutModalOpen] = useState(false);
 
     const closeMobileMenu = () => {
         setIsMobileMenuOpen(false);
     };
     return (
-        <header className={styles.header}>
-            <Link to="/" className={styles.headerLogo}>
-                <img src={Logo} alt="Logo" />
-            </Link>
+        <>
+            <header className={styles.header}>
+                <Link to="/" className={styles.headerLogo}>
+                    <img src={Logo} alt="Logo" />
+                </Link>
 
-            <nav
-                className={`${styles.headerNav} 
+                <nav
+                    className={`${styles.headerNav} 
             ${isMobileMenuOpen ? styles.headerNavOpen : ''}`}
-            >
-                <ul className={styles.menu}>
-                    <li className={styles.menuItem}>
-                        <Link to="/" onClick={closeMobileMenu}>
-                            Главная
-                        </Link>
-                    </li>
-                    <li className={styles.menuItem}>
-                        <a href="#" onClick={closeMobileMenu}>
-                            О проекте
-                        </a>
-                    </li>
-                    <li className={styles.menuItem}>
-                        <a href="#" onClick={closeMobileMenu}>
-                            Избранное
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-
-            <div className={styles.headerActions}>
-                <button
-                    className={styles.headerNavButton}
-                    onClick={() => setIsMobileMenuOpen((prev) => !prev)}
-                    aria-label="Открыть меню"
                 >
-                    {isMobileMenuOpen ? <X /> : <Menu />}
-                </button>
+                    <ul className={styles.menu}>
+                        <li className={styles.menuItem}>
+                            <Link to="/" onClick={closeMobileMenu}>
+                                Главная
+                            </Link>
+                        </li>
+                        <li className={styles.menuItem}>
+                            <a
+                                onClick={() => {
+                                    closeMobileMenu();
+                                    setAboutModalOpen(!aboutModalOpen);
+                                }}
+                            >
+                                О проекте
+                            </a>
+                        </li>
+                        <li className={styles.menuItem}>
+                            <a onClick={closeMobileMenu}>Избранное</a>
+                        </li>
+                    </ul>
+                </nav>
 
-                <button
-                    className={styles.headerTheme}
-                    onClick={onThemeToggle}
+                <div className={styles.headerActions}>
+                    <button
+                        className={styles.headerNavButton}
+                        onClick={() => setIsMobileMenuOpen((prev) => !prev)}
+                        aria-label="Открыть меню"
+                    >
+                        {isMobileMenuOpen ? <X /> : <Menu />}
+                    </button>
 
-                    aria-label="Изменить тему"
-                >
-                    {theme === 'dark' ? <Sun /> : <Moon />}
-                </button>
-            </div>
-        </header>
+                    <button
+                        className={styles.headerTheme}
+                        onClick={onThemeToggle}
+
+                        aria-label="Изменить тему"
+                    >
+                        {theme === 'dark' ? <Sun /> : <Moon />}
+                    </button>
+                </div>
+            </header>
+
+            <AboutModal isOpen={aboutModalOpen} onClose={() => setAboutModalOpen(false)} />
+        </>
     );
 };
 
